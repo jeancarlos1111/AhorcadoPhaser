7d18f6d7bd15d113b7d3f891f9df928c67565fda
jean <jeancarlos1111@gmail.com>
2026-07-05 21:50:09 -0400

    feat: add PWA support

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'ahorcado-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './js/main.js',
+  './js/constants.js',
+  './js/LanguageScene.js',
+  './js/GameScene.js',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
